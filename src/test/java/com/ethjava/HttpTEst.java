b12520679c72041973113ff527ea8a86339b36db
Administrator <1258012969@>
2018-04-27 13:41:49 +0800

    ERC20  web3j

--- src/test/java/com/ethjava/HttpTEst.java
+++ src/test/java/com/ethjava/HttpTEst.java
@@ -6,7 +6,6 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
-import sun.net.www.http.HttpClient;
 
 import java.io.IOException;
 
@@ -18,13 +17,13 @@ public class HttpTEst {
     public static void main(String[] args) {
         CloseableHttpClient httpCilent2 = HttpClients.createDefault();
         RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectTimeout(5000)   //设置连接超时时间
-                .setConnectionRequestTimeout(5000) // 设置请求超时时间de
-                .setSocketTimeout(5000)
+                .setConnectTimeout(15000)   //设置连接超时时间
+                .setConnectionRequestTimeout(15000) // 设置请求超时时间de
+                .setSocketTimeout(15000)
                 .setRedirectsEnabled(true)//默认允许自动重定向
                 .build();
-        HttpGet httpGet2 = new HttpGet("http://etherscan.io/tokens?p=3");
-        httpGet2.setConfig(requestConfig);
+        HttpGet httpGet2 = new HttpGet("https://api.ethplorer.io/getTopTokens?apiKey=freekey&criteria=cap");
+        //httpGet2.setConfig(requestConfig);
         String srtResult = "";
         try {
             HttpResponse httpResponse = httpCilent2.execute(httpGet2);
--- src/test/java/com/ethjava/TokenERC20Test.java
+++ src/test/java/com/ethjava/TokenERC20Test.java
@@ -2,14 +2,16 @@ package com.ethjava;
 
 import com.alibaba.fastjson.JSONObject;
 import com.ethjava.utils.Environment;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import erc20.TokenERC20;
-import jdk.nashorn.internal.runtime.JSONFunctions;
+import org.apache.commons.lang.StringUtils;
 import org.web3j.crypto.Credentials;
 import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
-import org.web3j.protocol.core.methods.response.*;
+import org.web3j.protocol.core.RemoteCall;
+import org.web3j.protocol.core.methods.response.EthBlock;
+import org.web3j.protocol.core.methods.response.EthBlockNumber;
+import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.Contract;
 import org.web3j.utils.Convert;
@@ -26,46 +28,60 @@ public class TokenERC20Test {
     public static void main(String[] args) throws Exception {
 
         Web3j web3j = Web3j.build(new HttpService(Environment.RPC_URL));
-
-
-        //load(web3j);
+        balanceOf(web3j,"0x315EDC75A0A1ECdab0D7887A4F17C931D498163f");
+        //deploy(web3j,"ERC20-03","ERC20-0");
+        //loadAndTransf(web3j,"0x315EDC75A0A1ECdab0D7887A4F17C931D498163f");
         tx(web3j);
     }
 
     private static void tx(Web3j web3j) throws java.io.IOException {
         EthBlockNumber number = web3j.ethBlockNumber().send();
         System.out.println(number.getBlockNumber());
-
         for (BigInteger i = BigInteger.ZERO; i.compareTo(number.getBlockNumber()) < 1; i = i.add(BigInteger.ONE)) {
-
             List<EthBlock.TransactionResult> transactions = web3j.ethGetBlockByNumber(DefaultBlockParameter.valueOf(i), true).send().getBlock().getTransactions();
-
-
             transactions.forEach(tx -> {
-                EthBlock.TransactionResult tr = (EthBlock.TransactionResult) tx.get();
-                Object o = tx.get();
-                System.out.println("==="+ JSONObject.toJSON(o));
+                EthBlock.TransactionObject tr = (EthBlock.TransactionObject) tx.get();
+
+                System.out.println(JSONObject.toJSON(tr));
+                String input = tr.getInput();
+                if(StringUtils.isNotEmpty(input) && input.startsWith("0xa9059cbb")){
+                    String contractCode = tr.getTo();
+                    System.out.println("合约地址:"+contractCode + "\t所在区块:"+ tr.getBlockNumber());
+                }
             });
         }
     }
 
-    /*
-    0x59b4a95b9f7e2612f43a6a26e1dcaf1f62cb87dd
-UTC--2018-04-26T08-04-54.362000000Z--59b4a95b9f7e2612f43a6a26e1dcaf1f62cb87dd.json
-     */
-//1.220951596568e+21
 
-    private static void load(Web3j web3j) throws Exception {
+    private static void loadAndTransf(Web3j web3j,String contractAddress) throws Exception {
 
         Credentials credentials = WalletUtils.loadCredentials("gaoxun", "D:/eth-test/wallet/UTC--2018-04-26T08-04-54.362000000Z--59b4a95b9f7e2612f43a6a26e1dcaf1f62cb87dd.json");
-        TokenERC20 contract = TokenERC20.load("0x4e8d37d9ba3f324b72bbb8049596ee8990ed01a9",web3j, credentials, Contract.GAS_PRICE, Contract.GAS_LIMIT);
+        TokenERC20 contract = TokenERC20.load(contractAddress,web3j, credentials, Contract.GAS_PRICE, Contract.GAS_LIMIT);
 
+        BigInteger balance1 = contract.balanceOf("0x426da2ac47fea9e5d68526bc6c7fbd449bba3e2a").send();
+        System.out.println("余额++:"+balance1);
 
         TransactionReceipt send = contract.transfer("0x426da2ac47fea9e5d68526bc6c7fbd449bba3e2a", BigInteger.valueOf(1000000L)).send();
-        System.out.println(JSONObject.toJSON(send));
+
+
+        BigInteger balance2 = contract.balanceOf("0x426da2ac47fea9e5d68526bc6c7fbd449bba3e2a").send();
+        System.out.println("余额==:"+balance2);
+
+        System.out.println("交易回执"+JSONObject.toJSON(send));
     }
 
-    private static void deploy(Web3j web3j) throws Exception {
+    private static void balanceOf(Web3j web3j,String contractAddress) throws Exception {
+
+        Credentials credentials = WalletUtils.loadCredentials("gaoxun", "D:/eth-test/wallet/UTC--2018-04-26T08-04-54.362000000Z--59b4a95b9f7e2612f43a6a26e1dcaf1f62cb87dd.json");
+        TokenERC20 contract = TokenERC20.load(contractAddress,web3j, credentials, Contract.GAS_PRICE, Contract.GAS_LIMIT);
+
+
+        BigInteger send = contract.balanceOf("0x426da2ac47fea9e5d68526bc6c7fbd449bba3e2a").send();
+        System.out.println(send);
+
+    }
+
+    private static void deploy(Web3j web3j,String tokenName,String tokenSymbol) throws Exception {
 
         BigInteger initialSupply = Convert.toWei(BigDecimal.valueOf(1000), Convert.Unit.GWEI).toBigInteger();
 
@@ -73,11 +89,9 @@ UTC--2018-04-26T08-04-54.362000000Z--59b4a95b9f7e2612f43a6a26e1dcaf1f62cb87dd.js
         TokenERC20 contract = TokenERC20.deploy(
                 web3j, credentials,
                 Contract.GAS_PRICE, Contract.GAS_LIMIT,
-                initialSupply,"ERC20-01","ERC20-01",BigInteger.ONE).send();  // constructor params
+                initialSupply,tokenName,tokenSymbol,BigInteger.ONE).send();  // constructor params
 
 
         System.out.println(JSONObject.toJSON(contract));
     }
-
-    //0x4e8d37d9ba3f324b72bbb8049596ee8990ed01a9
 }
